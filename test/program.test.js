81ba09586a5b52058ac88686682579b4a07400ac
Ocarinos <alexlepoupon@gmail.com>
2017-02-24 09:40:13 +0100

    Exercise 1

--- test/program.test.js
+++ test/program.test.js
@@ -9,4 +9,47 @@ describe('Function transformCheckpoint', function() {
     expect(transformCheckpoint()).to.be.false;
   });
 
+  it('Function transformCheckpoint with parameter should mutate the parameter', function() {
+  	
+  	var checkpoint = {
+	  id: 'whataw0nd3rful1d',
+	  uuid: 'whataw0nd3rful1d',
+	  address: 'unknown',
+	  addressType: 'unknown',
+	  connectable: true,
+	  advertisement: {
+	    localName: undefined,
+	    txPowerLevel: undefined,
+	    manufacturerData: undefined,
+	    serviceData: [],
+	    serviceUuids: [ 'abcd' ]
+	  },
+	  rssi: -66,
+	  services: null,
+	  state: 'outofcontrol'
+	};
+
+	var checkpointMutated = {
+	  id: 'whataw0nd3rful1d',
+	  uuid: 'whataw0nd3rful1d',
+	  address: 'unknown',
+	  addressType: 'unknown',
+	  connectable: true,
+	  advertisement: {
+	    localName: undefined,
+	    txPowerLevel: undefined,
+	    manufacturerData: undefined,
+	    serviceData: [],
+	    serviceUuids: [ 'abcd' ]
+	  },
+	  rssi: -66,
+	  services: null,
+	  state: 'outofcontrol'
+	};
+
+	transformCheckpoint(checkpointMutated);
+
+    expect(checkpoint).to.not.eql(checkpointMutated);
+  });
+
 });
